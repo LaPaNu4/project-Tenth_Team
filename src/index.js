14ee50e9e28931842797b4a65dbd76474a6e8b9a
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-06-02 05:23:02 +0300

    gn

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// import Pagination from 'tui-pagination'; https://www.npmjs.com/package/tui-pagination
+// import Notiflix from 'notiflix'; https://github.com/notiflix/Notiflix#readme
+// import _ from 'lodash'; | var _ = require('lodash'); https://lodash.com/
+// import axios from 'axios'; | const axios = require('axios').default; https://axios-http.com/uk/docs/example
