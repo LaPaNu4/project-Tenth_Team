c18aa4def3c61f73cd30516d0e607790d90539f6
SerhiiShapovalov <ShapovalovSV83@gmail.com>
2023-06-03 16:46:57 +0300

    Started work with the footer

---
+++ src/js/footer-modal.js
@@ -0,0 +1,44 @@
+
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open-contacts]'),
+  footerBackDrop: document.querySelector('.footer__backdrop'),
+  closeModalBtn: document.querySelector('[data-modal-close-contacts]'),
+};
+
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.footerBackDrop.addEventListener('click', onClickBackdrop);
+
+function onOpenModal() {
+  refs.backdrop.classList.remove('is-hidden');
+  window.addEventListener('keydown', onPressESC);
+}
+
+function onCloseModal() {
+  refs.backdrop.classList.add('is-hidden');
+  window.removeEventListener('keydown', onPressESC);
+}
+
+function onClickBackdrop(e) {
+  if (e.target.classList.contains('js-close-modal')) {
+    onCloseModal();
+  }
+}
+
+function onPressESC(e) {
+  if (e.keyCode === 27) {
+    onCloseModal();
+  }
+}
+
+const toggleContacts = () => {
+  const isContactsOpen =
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  openModalBtn.setAttribute('aria-expanded', !isMenuOpen);
+  mobileMenu.classList.toggle('is-open');
+
+  const scrollLockMethod = !isContactsOpen
+    ? 'disableBodyScroll'
+    : 'enableBodyScroll';
+  bodyScrollLock[scrollLockMethod](document.body);
+};
