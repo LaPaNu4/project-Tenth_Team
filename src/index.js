e6589748960c053469c25773362024c03b816566
Chekos10 <119345370+Chekos10@users.noreply.github.com>
2023-06-03 21:05:36 +0300

    save_first

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 // import Notiflix from 'notiflix'; https://github.com/notiflix/Notiflix#readme
 // import _ from 'lodash'; | var _ = require('lodash'); https://lodash.com/
 // import axios from 'axios'; | const axios = require('axios').default; https://axios-http.com/uk/docs/example
-
+import "./js/header.js";
 import "./js/catalog.js"
-
 import "./js/library-filter.js"
 
