0795d5ca4cb4a69c6fe99b508192b8f8225b33ef
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-06-04 12:22:06 +0300

    add weekly-trends

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@
 // import _ from 'lodash'; | var _ = require('lodash'); https://lodash.com/
 // import axios from 'axios'; | const axios = require('axios').default; https://axios-http.com/uk/docs/example
 
-import "./js/catalog.js"
+import './js/catalog.js';
 
-import "./js/library-filter.js"
+import './js/library-filter.js';
 
-import './js/footer-modal.js'
+import './js/footer-modal.js';
 
-import './js/library-II.js'
+import './js/library-II.js';
 
-import './js/rating.js'
+import './js/rating.js';
+
+import './js/weekly-trends.js';
