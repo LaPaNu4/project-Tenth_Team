41cf0e933def30192aaf0524899649fad34701b4
SerhiiShapovalov <ShapovalovSV83@gmail.com>
2023-06-04 18:29:02 +0300

    Footer + footer modal

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,44 +1,59 @@
-
-const refs = {
-  openModalBtn: document.querySelector('[data-modal-open-contacts]'),
-  footerBackDrop: document.querySelector('.footer__backdrop'),
-  closeModalBtn: document.querySelector('[data-modal-close-contacts]'),
-};
-
-refs.openModalBtn.addEventListener('click', onOpenModal);
-refs.closeModalBtn.addEventListener('click', onCloseModal);
-refs.footerBackDrop.addEventListener('click', onClickBackdrop);
-
-function onOpenModal() {
-  refs.backdrop.classList.remove('is-hidden');
-  window.addEventListener('keydown', onPressESC);
-}
-
-function onCloseModal() {
-  refs.backdrop.classList.add('is-hidden');
-  window.removeEventListener('keydown', onPressESC);
-}
-
-function onClickBackdrop(e) {
-  if (e.target.classList.contains('js-close-modal')) {
-    onCloseModal();
-  }
-}
-
-function onPressESC(e) {
-  if (e.keyCode === 27) {
-    onCloseModal();
-  }
-}
-
-// const toggleContacts = () => {
-//   const isContactsOpen =
-//     openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//   openModalBtn.setAttribute('aria-expanded', !isMenuOpen);
-//   mobileMenu.classList.toggle('is-open');
-
-//   const scrollLockMethod = !isContactsOpen
-//     ? 'disableBodyScroll'
-//     : 'enableBodyScroll';
-//   bodyScrollLock[scrollLockMethod](document.body);
+// const refs = {
+//   openFooterModalBtn: document.querySelector('[data-modal-open-contacts]'),
+//   footerBackDrop: document.querySelector('.footer-backdrop'),
+//   closeFooterModalBtn: document.querySelector('[data-modal-close-contacts]'),
+//   modalFooter: document.querySelector('[data-modal]'),
+//   body: document.querySelector('body'),
 // };
+
+// refs.openFooterModalBtn.addEventListener('click', onOpenFooterModal);
+// refs.closeFooterModalBtn.addEventListener('click', onCloseFooterModal);
+// refs.footerBackDrop.addEventListener('click', onClickFooterModalBackdrop);
+
+// function onOpenFooterModal() {
+//   refs.footerBackDrop.classList.remove('is-hidden');
+//   window.addEventListener('keydown', onPressESC);
+// }
+
+// function onCloseFooterModal() {
+//   refs.backdrop.classList.add('is-hidden');
+//   window.removeEventListener('keydown', onPressESC);
+// }
+
+// function onClickFooterModalBackdrop(e) {
+//   if (e.target.classList.contains('js-close-modal')) {
+//     onCloseFooterModal();
+//   }
+// }
+
+// function onPressESC(e) {
+//   if (e.keyCode === 27) {
+//     onCloseFooterModal();
+//   }
+// }
+
+//   refs.openModalBtnF.addEventListener('click', toggleModal);
+//   refs.closeModalBtnF.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modalFooter.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// // ********************************Внизу полура
+(() => {
+  const refs = {
+    openModalBtnF: document.querySelector('[data-modal-open-contacts]'),
+    closeModalBtnF: document.querySelector('[data-modal-close-contacts]'),
+    modalFooter: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtnF.addEventListener('click', toggleModal);
+  refs.closeModalBtnF.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modalFooter.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+  
+})();
