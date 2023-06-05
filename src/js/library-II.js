65ddc19787b3a10384718f92845927bde9888632
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-06-05 21:49:32 +0300

    Updates_js_library

--- src/js/library-II.js
+++ src/js/library-II.js
@@ -1,10 +1,10 @@
 const { all } = require("axios");
 
 const gallery = document.querySelector('.movies-galery');
-const selectGenre = document.querySelector('.genres');
 const searchMore = document.querySelector('.search-more');
 const loadMore = document.querySelector('.load');
 const emptyGallery = document.querySelector('.empty-gallery');
+const dropDown = document.querySelector('.dropdown-library');
 
 
 //adding dropdown functionality
@@ -44,11 +44,13 @@ const emptyGallery = document.querySelector('.empty-gallery');
 const STORAGE = 'favoriteMovies';
 
 loadMore.classList.add('hide');
-selectGenre.classList.add('hide');
 emptyGallery.classList.add('hide');
+dropDown.classList.add('hide');
+
 
 const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 const API_KEY = '14b16a10583a3d9315723a356100e4ad';
+const renderedMovies = [];
 
 function fetchFromLibrary() {
     const movieIds = JSON.parse(localStorage.getItem(STORAGE));
@@ -63,51 +65,37 @@ function fetchFromLibrary() {
         fetch(`${BASE_URL}?api_key=${API_KEY}&${movie_id}`)
         .then(data => data.json())
         .then(movieData => {
+            renderedMovies.push(movieData);
             const moviesById = movieMarkUp(movieData);
             gallery.insertAdjacentHTML('beforeend', moviesById);
             loadMore.classList.add('show');
             loadMore.classList.remove('hide');
-        })
-    })    
+            dropDown.classList.add('show');
+            dropDown.classList.remove('hide');
+        })            
         .catch(error => {
         console.log(error);
         throw new Error(error);
         });
+    })
+    filterMoviesByGenre(renderedMovies);
 }
 
+const filterMoviesByGenre = (movies) => {
+    const genre = selectGenre.value;
 
-// function getAllStoredMovies() {
-//     //e.preventDefault();
-//     // const movie = {
-//     //     img: img,
-//     //     title: title,
-//     //     genre: genre,
-//     //     year: year,
-//     //     rating: rating,
-//     // };
-
-//     const movies = JSON.parse(localStorage.getItem(STORAGE));
-//     console.log(movies);
-//     let movieId;
-
-//     if(movies) {
-//         return movieId = movies.map(item => {
-//             item.movie_id;
-//         });
-//     }
-// }
-
-// const filterMoviesByGenre = (movies) => {
-//     const genre = selectGenre.value;
-
-//     const chosenMovies = movies.filter(movie => movie.genre === genre);
-//     if(chosenMovies.length > 0) {
-//         const galleryItems = movieMarkUp(chosenMovies);
-//         gallery.insertAdjacentHTML('beforeend', galleryItems);
-//         loadMore.classList.add('show');
-//         loadMore.classList.remove('hide');
-//     }
-// }    
+    const chosenMovies = movies.filter(movie => movie.genre === genre);
+    if(chosenMovies.length > 0) {
+        const galleryItems = movieMarkUp(chosenMovies);
+        gallery.insertAdjacentHTML('beforeend', galleryItems);
+        loadMore.classList.add('show');
+        loadMore.classList.remove('hide');
+    } else {
+        gallery.innerHTML = '';
+        loadMore.classList.add('hide');
+        loadMore.classList.remove('show');
+    }
+}    
 
  const movieMarkUp = (dataComing) => {
     // return dataComing.map(item => {
@@ -124,11 +112,7 @@ function fetchFromLibrary() {
 ).join('');
 }
 
-// const emptyLibrary = () => {
-//     searchMore.classList.add('show');
-//     searchMore.classList.remove('hide');
-//     selectGenre.classList.add('show');
-
-// }
+window.onload = fetchFromLibrary;
+dropDown.addEventListener('click', filterMoviesByGenre);
 
 
