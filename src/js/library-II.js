ab50ae83a73894a50c1c3778ca4c6c14019ea63a
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-06-05 16:16:20 +0300

    Local_Storage_Library_updates

--- src/js/library-II.js
+++ src/js/library-II.js
@@ -39,50 +39,74 @@ const emptyGallery = document.querySelector('.empty-gallery');
 
 
 
-
-
-
 const movies = [];
-const STORAGE = 'movies-from-storage';
+const STORAGE = 'favoriteMovies';
 
 //loadMore.classList.add('hide');
 // selectGenre.classList.add('hide');
 //emptyGallery.classList.add('hide');
+const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = '14b16a10583a3d9315723a356100e4ad';
+
+function fetchFromLibrary(movieId) {
+    const query = `${BASE_URL}?api_key=${API_KEY}?${movieId}`;
+
+    return fetch(query)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+        })
+        .then(data => {
+        return data;
+        })
+        .catch(error => {
+        console.log(error);
+        throw new Error(error);
+        });
+}
 
 
-const getStoredMovies = (e) => {
+const getAllStoredMovies = (e) => {
     e.preventDefault();
-
-    const movie = {
-        img: img,
-        title: title,
-        genre: genre,
-        year: year,
-        rating: rating,
-    };
-
+    // const movie = {
+    //     img: img,
+    //     title: title,
+    //     genre: genre,
+    //     year: year,
+    //     rating: rating,
+    // };
 
     const movies = localStorage.parse(localStorage.getItem(STORAGE));
+    console.log(movies);
+    let movieId;
 
     if(movies) {
-        const genre = selectGenre.value;
-        const chosenMovies = movies.filter(movie => movie.genre);
+        return movieId = movies.map(item => {
+            item.movie_id;
+        });
+    }
+}
 
+const filterMoviesByGenre = (movies) => {
+    const genre = selectGenre.value;
+
+    const chosenMovies = movies.filter(movie => movie.genre);
+    if(genre === chosenMovies) {
         const galleryItems = movieMarkUp(chosenMovies);
         gallery.insertAdjacentHTML('beforeend', galleryItems);
         loadMore.classList.add('show');
         loadMore.classList.remove('hide');
     }
-    console.log('something');
-    emptyLibrary();
 }
+    
 
 const movieMarkUp = (dataComing) => {
-
     return dataComing.map(item => {
-        const {img, title, genre, year, rating} = item;
+        const {img, title, genre, year, rating, id} = item;
         `
-        <div class="movie">
+        <div class="movie" id=${id}>
             <img class="movie-img" src="${img}">
             <h2 class="movie-title">${title}</h2>
             <h2 class="movie-genre">${genre}</h2>
