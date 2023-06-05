2d0d575e22a1c07a49d34a451d38d9dc648dbc2e
LarisaBeskrovna <117515454+LarisaBeskrovna@users.noreply.github.com>
2023-06-05 11:11:31 +0300

    js

--- src/js/light-theme.js
+++ src/js/light-theme.js
@@ -15,6 +15,8 @@ document.querySelector('.themetoggle').addEventListener('click', (event) => {
               document.querySelector('body').classList.add('light');
               document.querySelector('.main-header').classList.add('light');
               document.querySelector('.mobile-menu').classList.add('light');
+              document.querySelector('.button-subject').style.display = "none";
+              document.querySelector('.button-subjectlight').style.display = "flex";
               document.querySelectorAll('.mobile-menu-link').forEach(mobileItem=>{
                 mobileItem.classList.add('lightblack')
               })
@@ -23,12 +25,13 @@ document.querySelector('.themetoggle').addEventListener('click', (event) => {
               document.querySelector('.menu-btn-mobile').classList.add('lightcolor');
               document.querySelector('.footer-copyright').classList.add('lightcolor');
               document.querySelector('.footer-copyright--btn').classList.add('lightcolor');
-              document.querySelector('.button-subject').style.display = "none";
-              document.querySelector('.button-subjectlight').style.display = "flex";
+              
             }
             else {
                 document.querySelector('body').classList.remove('light');
                 document.querySelector('.mobile-menu').classList.remove('light');
+                document.querySelector('.button-subjectlight').style.display = "none";
+                document.querySelector('.button-subject').style.display = "flex";
                 document.querySelectorAll('.mobile-menu-link').forEach(mobileItem=>{
                   mobileItem.classList.remove('lightblack');
                 })
@@ -38,8 +41,7 @@ document.querySelector('.themetoggle').addEventListener('click', (event) => {
                 document.querySelector('.main-header').classList.remove('light');
                 document.querySelector('.footer-copyright').classList.remove('lightcolor');
                 document.querySelector('.footer-copyright--btn').classList.remove('lightcolor');
-                document.querySelector('.button-subjectlight').style.display = "none";
-                document.querySelector('.button-subject').style.display = "flex";
+                
               }
             } catch (err) { }
     }
