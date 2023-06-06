f8f8c78b6d459613ddbfd2848bf813e29ca01c6d
SergiiS95 <SSirota1995@gmail.com>
2023-06-06 19:57:43 +0300

    update js

--- src/js/upcomingThisMounth.js
+++ src/js/upcomingThisMounth.js
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import { result } from 'lodash';
 
 const URL = 'https://api.themoviedb.org/3/movie/upcoming';
 const API_KEY = '0b7ed72c0b07bc683ffc3b8a0e430792';
@@ -9,18 +10,17 @@ const upcomingFilm = document.querySelector('.upcoming-film');
 const favoriteMovies = 'favoriteMovies';
 
 
-async function getUpcomingData () {
-    const response = await axios.get(`${URL}?api_key=${API_KEY}`);
-    const result = response.data.results;
+async function getUpcomingData() {
+  const response = await axios.get(`${URL}?api_key=${API_KEY}`);
+  const result = response.data.results;
 
-    return result
+  return result;
 }
 
 async function getGenresById(genreIds) {
+  const response = await axios.get(`${GENRE_URL}?api_key=${API_KEY}`);
+  const data = response.data.genres;
 
-const response = await axios.get(`${GENRE_URL}?api_key=${API_KEY}`);
- const data = response.data.genres;
-  
   const genreNames = genreIds.map(genreId => {
     const genre = data.find(genre => genre.id === genreId);
     return genre.name;
@@ -29,64 +29,54 @@ const response = await axios.get(`${GENRE_URL}?api_key=${API_KEY}`);
   return genreNames.join(', ');
 }
 
-
-getGenresById()
-
 async function getUpcoming() {
-    try {
-        const data = await getUpcomingData();
+  try {
+    const data = await getUpcomingData();
 
-      if (data.length === 0) {
-        renderMarkupError();
-        return;
-      }
+    if (data.length === 0) {
+      renderMarkupError();
+      return;
+    }
 
-      const randomFilm = Math.floor(Math.random() * data.length);
-        const renderFilm = data[randomFilm];
-       const idFilm = renderFilm.id;
-      const render = await createMarcup(renderFilm);
-        renderMarkup(render);
-        
-        const upcomingBtn = document.querySelector('.upcoming_content_btn');  
-        let savedLocal = localStorage.getItem('favoriteMovies');
-        let parsedLocal = JSON.parse(savedLocal);
-        parsedLocal = parsedLocal ? parsedLocal : [];   
+    const randomFilm = Math.floor(Math.random() * data.length);
+    const renderFilm = data[randomFilm];
+    const idFilm = renderFilm.id;
+    const render = await createMarkup(renderFilm);
+    renderMarkup(render);
 
-      if (parsedLocal.includes(idFilm)) {
+    const upcomingBtn = document.querySelector('.upcoming_content_btn');
+    let savedLocal = localStorage.getItem('favoriteMovies');
+    let parsedLocal = JSON.parse(savedLocal);
+    parsedLocal = parsedLocal ? parsedLocal : [];
+
+    if (parsedLocal.includes(idFilm)) {
+      upcomingBtn.textContent = 'Remove from my library';
+    } else {
+      upcomingBtn.textContent = 'Add to my library';
+    }
+
+    upcomingBtn.addEventListener('click', () => {
+      let savedLocal = localStorage.getItem('favoriteMovies');
+      let parsedLocal = JSON.parse(savedLocal);
+      parsedLocal = parsedLocal ? parsedLocal : [];
+
+        if (!parsedLocal.includes(idFilm)) {
+        parsedLocal.push(idFilm);
+        localStorage.setItem(favoriteMovies, JSON.stringify(parsedLocal));
         upcomingBtn.textContent = 'Remove from my library';
       } else {
+        let index = parsedLocal.findIndex(id => id === idFilm);
+        parsedLocal.splice(index, 1);
+        localStorage.setItem(favoriteMovies, JSON.stringify(parsedLocal));
         upcomingBtn.textContent = 'Add to my library';
       }
-
-        upcomingBtn.addEventListener('click', () => {
-          
-  let savedLocal = localStorage.getItem('favoriteMovies');
-  let parsedLocal = JSON.parse(savedLocal);
-  parsedLocal = parsedLocal ? parsedLocal : [];
-
-  if (!parsedLocal.includes(idFilm)) {
-    parsedLocal.push(idFilm);
-    localStorage.setItem(favoriteMovies, JSON.stringify(parsedLocal));
-    upcomingBtn.textContent = 'Remove from my library';
-  } else {
-    let index = parsedLocal.findIndex(id => id === idFilm);
-    parsedLocal.splice(index, 1);
-    localStorage.setItem(favoriteMovies, JSON.stringify(parsedLocal));
-    upcomingBtn.textContent = 'Add to my library';
+    });
+  } catch (error) {
+    console.log(error);
   }
-      });
-    
-    
-    } catch (error) {
-      console.log(error);
-    }
 }
 
-getUpcoming();
-
-  
-
-async function createMarcup({
+async function createMarkup({
   backdrop_path,
   genre_ids,
   title,
@@ -96,72 +86,67 @@ async function createMarcup({
   release_date,
   popularity,
 }) {
-    const genreNames = await getGenresById(genre_ids);
+  const genreNames = await getGenresById(genre_ids);
 
-     return `
-      <img
-        class="upcoming_img"
-        src="https://image.tmdb.org/t/p/original${backdrop_path}"
-        alt="Poster ${title}"
-       
-      
-      />
+  return `
+    <img
+      class="upcoming_img"
+      src="https://image.tmdb.org/t/p/original${backdrop_path}"
+      alt="Poster ${title}"
+    />
 
-      <div class="upcoming_content">
-        <h3 class="upcoming_name">${title}</h3>
+    <div class="upcoming_content">
+      <h3 class="upcoming_name">${title}</h3>
 
-        <div class="upcoming_thumb">
-          <ul class="upcoming-list left">
-            <li class="upcoming_list_item">
-              <p class="upcoming_list_text color-h">Release date</p>
-              <p class="upcoming_list_date color-p">${release_date}</p>
-            </li>
-            <li class="upcoming_list_item">
-              <p class="upcoming_list_text color-h">Vote / Votes</p>
-              <p class="upcoming_list_vote">
-                <span class="vote"> ${vote_average}</span> / <span class="vote">${vote_count}</span>
-              </p>
-            </li>
-          </ul>
+      <div class="upcoming_thumb">
+        <ul class="upcoming-list left">
+          <li class="upcoming_list_item">
+            <p class="upcoming_list_text color-h">Release date</p>
+            <p class="upcoming_list_date color-p">${release_date}</p>
+          </li>
+          <li class="upcoming_list_item">
+            <p class="upcoming_list_text color-h">Vote / Votes</p>
+            <p class="upcoming_list_vote">
+              <span class="vote">${vote_average}</span> / <span class="vote">${vote_count}</span>
+            </p>
+          </li>
+        </ul>
 
-          <ul class="upcoming-list rigth">
-            <li class="upcoming_list_item ">
-              <p class="upcoming_list_text color-h">Popularity</p>
-              <p class="upcoming_list_pop">${popularity.toFixed(1)}</p>
-            </li>
-            <li class="upcoming_list_item">
-              <p class="upcoming_list_text color-h">Genre</p>
-              <p class="upcoming_list_genre">${genreNames}</p>
-            </li>
-          </ul>
-        </div>
+        <ul class="upcoming-list right">
+          <li class="upcoming_list_item">
+            <p class="upcoming_list_text color-h">Popularity</p>
+            <p class="upcoming_list_pop">${popularity.toFixed(1)}</p>
+          </li>
+          <li class="upcoming_list_item">
+            <p class="upcoming_list_text color-h">Genre</p>
+            <p class="upcoming_list_genre">${genreNames}</p>
+          </li>
+        </ul>
+      </div>
 
-        <h3 class="upcoming_content_title color-h">About</h3>
-        <p class="upcoming_content_text">${overview}
-        </p>
+      <h3 class="upcoming_content_title color-h">About</h3>
+      <p class="upcoming_content_text">${overview}</p>
 
-  <button type="button" class="upcoming_content_btn" id="addLibrary">
-  Add to my library
-  </button>
-`;
+      <button type="button" class="upcoming_content_btn" id="addLibrary">
+         Add to my library
+      </button>
+    </div>
+  `;
 }
+
 
 function renderMarkup(markup) {
   upcomingFilm.innerHTML = markup;
 }
 
-
-
-
 function renderMarkupError() {
   upcomingFilm.innerHTML = `<div class="upcoming-error-container">
-        <p class="upcoming_error_container_text">
-          OOPS...<br />
-          We are very sorry!<br />
-          But we couldn't find any upcoming movies this month.
-        </p>
-      </div>`;
+    <p class="upcoming_error_container_text">
+    OOPS...<br />
+    We are very sorry!<br />
+    But we couldn't find any upcoming movies this month.
+    </p>
+  </div>`;
 }
 
-
-
+getUpcoming();
