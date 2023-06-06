2c43ad1726d4f542fa4e882f6c6d8ed61b552e7f
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-06-07 01:00:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 // import Notiflix from 'notiflix'; https://github.com/notiflix/Notiflix#readme
 // import _ from 'lodash'; | var _ = require('lodash'); https://lodash.com/
 // import axios from 'axios'; | const axios = require('axios').default; https://axios-http.com/uk/docs/example
-// import './js/hero-random-film.js';
-// import './js/header.js';
-// import './js/light-theme.js';
-import "./js/catalog.js";
+import './js/hero-random-film.js';
+import './js/header.js';
+import './js/light-theme.js';
+// import "./js/catalog.js";
 // import './js/library-filter.js';
 
 import './js/footer-modal.js';
