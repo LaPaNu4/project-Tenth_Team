93335a655687a3591356200e1d7021c8267363f0
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-06-08 14:14:51 +0300

    add stars weekly

--- src/js/weekly-trends.js
+++ src/js/weekly-trends.js
@@ -58,7 +58,6 @@ function renderMarkup(results) {
   });
 }
 function createMarkup(results) {
-  // console.log(results);
   return results
     .slice(0, 3)
     .map(
@@ -70,14 +69,14 @@ function createMarkup(results) {
         vote_average,
         id,
       }) => {
-        rating = vote_average
+        rating = vote_average;
         let posterIMG = ``;
         if (poster_path) {
           posterIMG = `${IMG_BASE_URL}${IMG_W400}${poster_path}`;
         } else {
           posterIMG = 'https://i.ibb.co/C0LFwTh/OIF.jpg';
         }
-        
+
         let starIcons = '';
         for (let i = 1; i <= 5; i++) {
           let starClass = 'fa-star-o';
@@ -88,40 +87,41 @@ function createMarkup(results) {
           }
           starIcons += `<span class="fa star ${starClass}"> </span>`;
         }
-        return ` <li class='gallery-weekly__list-elem hover-cursor' data-catalog-item id='${id}'>           
+
+        // Calculate the width based on the rating value
+        const width = Math.round(rating) * 10;
+
+        return `<li class='gallery-weekly__list-elem hover-cursor' data-catalog-item id='${id}'>
           <img class='gallery-weekly__image' src="${posterIMG}" alt="${original_title}" loading="lazy" id='${id}'>
-         <div class='gallery-weekly__all-info'> 
-          <div class="gallery-weekly__info">
+          <div class='gallery-weekly__all-info'>
+            <div class="gallery-weekly__info">
               <h3 class= 'gallery-weekly__title'>${original_title}</h3>
               <div class='cards-list_second_line'>
                 <div class='cards-list__text color-h'>
                   <p>${genre_ids} | ${release_date}</p>
                 </div>
               </div>
+            </div>
           </div>
-          
-        </div>
-        <div class="form_item form_item_weekly">
+          <div class="form_item form_item_weekly">
             <div class="form_lebel"> </div>
             <div data-ajax="true" class="rating rating_weekly  rating_set rating-hero">
-                <div class="rating_body">
-                    <div class="rating_active"></div>
-                    <div class="rating_items">
-                        <input type="radio" class="rating_item" value="1" name="rating">
-                        <input type="radio" class="rating_item" value="2" name="rating">
-                        <input type="radio" class="rating_item" value="3" name="rating">
-                        <input type="radio" class="rating_item" value="4" name="rating">
-                        <input type="radio" class="rating_item" value="5" name="rating">
-                    </div>
+              <div class="rating_body">
+                <div class="rating_active ${id}" style="width: ${width}%;"></div>
+                <div class="rating_items">
+                  <input type="radio" class="rating_item" value="1" name="rating">
+                  <input type="radio" class="rating_item" value="2" name="rating">
+                  <input type="radio" class="rating_item" value="3" name="rating">
+                  <input type="radio" class="rating_item" value="4" name="rating">
+                  <input type="radio" class="rating_item" value="5" name="rating">
                 </div>
+              </div>
             </div>
-        </div>
-      </li>`;
-      }, 
+          </div>
+        </li>`;
+      }
     )
     .join('');
-  
-  
 }
 export const getWeeklyTrends = getTrendData().then(({ results }) => {
   return renderMarkup(results);
